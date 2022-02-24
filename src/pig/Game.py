82a602fb19bbe7daf3c9ce524aee99c921534e0a
Asom98 <kaseemsh985@gmail.com>
2022-02-24 13:10:10 +0100

    I have made the player class and added some of the important functions to it

--- src/pig/Game.py
+++ src/pig/Game.py
@@ -1,14 +1,22 @@
-'''
+"""
+
 This is going to be the game file(class) where all the funktions are executed
 
 in the main function, those funktions will be imported from the other classes in the same package
 
 that will build the pig game!.
-'''
+
+"""
+
+import player
+
 
 def main():
+    player1 = player.Player("kassem")
+    player1.change_name("hassan")
+    print(player1.get_name())
+    pass
 
-    return 0;
 
 
 if __name__ == '__main__':
---
+++ src/pig/Player.py
@@ -0,0 +1,31 @@
+"""
+In this class the player instance will be made and
+
+to be able to use it in the game.
+
+Each player will have a name and will be able to change their name
+
+when ever they want
+"""
+
+class Player():
+    """Player class"""
+    
+    def __init__(self, name):
+        """Make an instance of the player class"""
+        #Name of the player
+        self.name = name
+    
+
+    def change_name(self, name):
+        """changing the name of the player"""
+        self.name = name
+        print(f"The name has been changed to: {self.name}")
+
+
+    def get_name(self):
+        """Getting the name of the player"""
+        return self.name
+
+
+    
