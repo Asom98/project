7175c47f588c9b773f77ec17286c590f01078037
Asom98 <kaseemsh985@gmail.com>
2022-02-24 11:38:52 +0100

    constructing the main Class with the main function and adding a doc in the beginning

---
+++ src/pig/Game.py
@@ -0,0 +1,16 @@
+'''
+This is going to be the game file(class) where all the funktions are executed
+
+in the main function, those funktions will be imported from the other classes in the same package
+
+that will build the pig game!.
+'''
+
+def main():
+    
+    return 0;
+
+
+if __name__ == '__main__':
+    # Call the main function.
+    main()
