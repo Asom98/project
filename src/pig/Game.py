e7c7294412b978c1768980bb4d4ba230620e9820
Asom98 <kaseemsh985@gmail.com>
2022-03-02 14:25:04 +0100

    Adding info to the information func

--- src/pig/Game.py
+++ src/pig/Game.py
@@ -21,12 +21,21 @@ def main():
 
 def information():
     """In this function will all the information needed to play the game be printed"""
+
     print("======================== Pig ========================")
     print("=============== Wellcome to pig game! ===============")
     print("")
     print("You will be provided with all the informations to play the game.")
     print("The game consist of either tow players or you can play with an AI(computer).")
     print("you will be able to choose a name and change it later(chooseable!).")
+    print("The object of the game to be the first player to reach 100 points, and ")
+    print("the points are scored by rolling a dice 1-6 and avoid rolling a 1.")
+    print("Players can roll as many as they want but if they get a 1 the points ")
+    print("sets to 0, the player chooses to stop rolling and gather the points and so on.")
+    print("Be carefull getting a 1 will erase all the points that you have!")
+    print("The score will be tracked and the first player to reache 100 point will win.")
+    print("")
+    print("======================= Enjoy =======================")
 
     
 
