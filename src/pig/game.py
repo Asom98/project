62a6db304ac82b157de00ca76a568f37c2c9dcde
Simon Eriksson <simon_eriksson_90@outlook.com>
2022-03-04 14:20:03 +0100

    Fixed some stuff. Tried cleaning it up to make it easier to read

--- src/pig/game.py
+++ src/pig/game.py
@@ -7,27 +7,7 @@ in the main function, those functions will be imported from the other classes in
 that will build the pig game!.
 
 
-
-
-import player
-import pickle
-import os
-
-
-
-def main():
-
-    player_name = 'Laura'
-
-    # Check if serialized object for player with name
-    # "Laura" exists
-    if os.path.exists('players/' + player_name + '.pkl'):
-        with open('players/' + player_name + '.pkl', 'rb') as f:
-            # player_laura is an instance of Player
-            player_laura = pickle.load(f)
-
 """
-
 
 import dice
 import diceHand
@@ -37,7 +17,6 @@ import highScore
 
 def main():
 
-
     set_name = True
 
     information()
@@ -45,16 +24,14 @@ def main():
     mode = game_mode() #choosing the game mode
 
     while(mode == 1):  #player against AI
-
-        print("player against AI")
+        print("Player against AI")
         break
 
     while(mode == 2):  #palyer vs another
-
         while(set_name):
             
-            name1 = input("First player! set a name here>> ")
-            name2 = input("Seconed player! set a name here>> ")
+            name1 = input("First player! Set your name here >> ")
+            name2 = input("Second player! Set your name here >> ")
             print        ("==================================")
             print        ("|")
             print        ("|")
@@ -67,38 +44,32 @@ def main():
             diceHand1 = diceHand.DiceHand()
             diceHand2 = diceHand.DiceHand()
     
-
-
             set_name = False
 
-        
         
         want_roll = True
 
         while (want_roll):
-
             print('(', player1.get_name(),')', "turn to roll a dice: ")
             value1 = dice1.roll()
             print("You rolled: " , value1)
 
             if value1 == 1:
-
-                print("OBS! you have rolled a 1. All your points will be erased!")
-                print("and it will be your opponent turn.")
+                print("OBS! You have rolled a 1. All your points will be erased!")
+                print("It is now your opponent's turn.")
                 print("|")
                 print("|")
                 diceHand1.add_dice(value1)
                 want_roll = False
             else:
-
                 diceHand1.add_dice(value1)
                 answer = input("Do you want to roll agin? y/yes, n/no >> ")
                 print("|")
                 print("|")
+
                 if answer == "y":
                     continue
                 else:
-
                     want_roll = False
                     print("|")
                     print("|")
@@ -112,7 +83,6 @@ def main():
             print("You rolled: " , value2)
 
             if value2 == 1:
-
                 print("OBS! you have rolled a 1. All your points will be erased!")
                 print("and it will be your opponent turn.")
                 print("|")
@@ -131,73 +101,22 @@ def main():
                     print("")
                     print("")
 
-    """
-    #palyer_playee1 = player.Player("Stina")
-
-    
-
-    
-
-
-
-
-        dice1 = Dice.Dice()
-    diceHand1 = diceHand.DiceHand()
-    dice1 = Dice.Dice()
-    DiceHand1 = DiceHand.DiceHand()
-
-    DiceHand1.add_dice(dice1.roll())
-    DiceHand1.add_dice(dice1.roll())
-    DiceHand1.add_dice(dice1.roll())
-    DiceHand1.add_dice(dice1.roll())
-    DiceHand1.add_dice(dice1.roll())
-    DiceHand1.add_dice(dice1.roll())
-    DiceHand1.add_dice(dice1.roll())
-    DiceHand1.add_dice(dice1.roll())
-
-    #DiceHand1.show_dice_hand()
-    print("  ")
-    print("  ")
-    dice2 = Dice.Dice()
-    DiceHand2 = DiceHand.DiceHand()
-
-    DiceHand2.add_dice(dice2.roll())
-    DiceHand2.add_dice(dice2.roll())
-    DiceHand2.add_dice(dice2.roll())
-    DiceHand2.add_dice(dice2.roll())
-    DiceHand2.add_dice(dice2.roll())
-    DiceHand2.add_dice(dice2.roll())
-    DiceHand2.add_dice(dice2.roll())
-    DiceHand2.add_dice(dice2.roll())
-
-    #DiceHand2.show_dice_hand()
-
-
-    high = HighScore.HighScore()
-
-    
-    print(high.serach_for_high(DiceHand1.dice_hand, DiceHand2.dice_hand))
-
-
-
-    """
 
 
 def information():
     """In this function will all the information needed to play the game be printed"""
 
     print("===================== Pig game ======================")
-    print("=============== Wellcome to pig game! ===============")
+    print("=============== Welcome to pig game! ===============")
     print("")
-    print("You will be provided with all the informations to play the game.")
-    print("The game consist of either tow players or you can play with an AI(computer).")
-    print("you will be able to choose a name and change it later(chooseable!).")
-    print("The object of the game to be the first player to reach 100 points, and ")
-    print("the points are scored by rolling a dice 1-6 and avoid rolling a 1.")
-    print("Players can roll as many as they want but if they get a 1 the points ")
-    print("sets to 0, the player chooses to stop rolling and gather the points and so on.")
-    print("Be carefull getting a 1 will erase all the points that you have!")
-    print("The score will be tracked and the first player to reache 100 point will win.")
+    print("You will be provided with all the information needed to play the game.")
+    print("The game consists of either two players, or you can play with an AI(computer).")
+    print("You will be able to choose a name and change it later(chooseable!).")
+    print("The object of the game is to be the first player to reach 100 points. ")
+    print("The points are scored by rolling a dice 1-6 and avoid rolling a 1.")
+    print("Players can roll as many times as they want, but if they roll a 1 the points ")
+    print("of your current turn gets set to 0. The player can choose to stop rolling and gather the points at any time.")
+    print("The score will be tracked and the first player to reach 100 points will win.")
     print("")
     print("======================= Enjoy =======================")
     print("")
@@ -208,19 +127,25 @@ def game_mode():
     """Here the player will choose to play with an AI or another player"""
 
     #the func will return 1 or 2 which determines the game mode
-    print("If you wanna play alone press (1). do you wanna play with another player press (2): ")
+    print("If you want to play alone press (1). If you want to play versus another player press (2): ")
     print("")
 
+
+
+
     try:
-        choice = int(input("please enter 1 or 2 >> "))
+        choice = int(input("Please enter 1 or 2 >> "))
     except ValueError:
-        print("You have entered a none integer value try again!")
-
-
-    while(choice > 2 or choice <= 0):
+        print("You have entered an invalid value. Try again!")
+    except TypeError:
+        print("You have entered an invalid value. Try again!")
+    except:
+        print("You have entered an invalid value. Try again!")
         
-        choice = input("please enter 1 or 2 >> ")
-    
+
+    while choice > 2 or choice <= 0:
+         choice = int(input("Please enter 1 or 2 >> "))
+
     return choice
 
 
@@ -239,9 +164,13 @@ def end_game():
     if player_bob.score > player_bob.highscore:
         player_bob.highscore = player_bob.score
 
+
+"""
     # Write player to file at the end of the game
     with open('players/Bob.pkl', 'wb') as f:
         pickle.dump(f, player_bob)
+"""
+
 
 if __name__ == '__main__':
     # Calling the main function to execute the game.
