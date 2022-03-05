4d9a5a92b763829c1ff77ab717dc875b1180cd40
Simon Eriksson <simon_eriksson_90@outlook.com>
2022-03-05 10:17:33 +0100

    added constructor and methods

--- src/pig/intelligence.py
+++ src/pig/intelligence.py
@@ -1,1 +1,33 @@
+"""This class is used if the player picks gamemode (1). 
+It should roll the dice by itself.
+It should have 2 difficulty levels, one normal and one hard.
 
+Add AI decision making, if there is time. 
+Ex. if the computer manages to collect enough points, it should stop rolling.
+
+Possibility of having the harder difficulty AI be more risk-averse.
+Stopping it's turn faster than it's normal counterpart.
+
+"""
+
+# AI always has the same name
+
+name = "Computer"
+
+class Intelligence():
+
+
+    def __init__(self):
+        pass
+
+    # Behavior for normal difficulty AI
+    def normal_difficulty():
+        pass
+
+    # Behavior for hard difficulty AI
+    def hard_difficulty():
+        pass
+
+    # General behavior
+    def action():
+        pass
