ceb45edb2715f4111dbe68943608f0565f900073
Simon Eriksson <simon_eriksson_90@outlook.com>
2022-03-09 11:33:38 +0100

    adding some stuff, more work needed

--- src/pig/intelligence.py
+++ src/pig/intelligence.py
@@ -1,4 +1,5 @@
-"""This class is used if the player picks gamemode (1). 
+"""
+This class is used if the player picks gamemode (1). 
 It should roll the dice by itself.
 It should have 2 difficulty levels, one normal and one hard.
 
@@ -7,34 +8,41 @@ Ex. if the computer manages to collect enough points, it should stop rolling.
 
 Possibility of having the harder difficulty AI be more risk-averse.
 Stopping it's turn faster than it's normal counterpart.
-
 """
+
+import dice
+
 
 class Intelligence():
 
-    # AI always has the same name
-
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        # name can be 'Computer (Normal) or Computer (Hard)
+        self.name = name 
 
 
     # General behavior
+    # This function might not be needed. Keeping it for now
     def action(self, roll):
         print(f"Computer rolls a: {roll}")
 
 
     # Behavior for normal difficulty AI
     def normal_difficulty(self):
+        """Picking this option sets ai name to 'Computer (Normal)"""
+        """This difficulty has the same rolling behaviour as player"""
+
         print("You have picked 'normal' difficulty!")
-        
-        
+                
 
     # Behavior for hard difficulty AI
     def hard_difficulty(self):
+        """Picking this option sets ai name to 'Computer (Hard)"""
+        """This difficulty is almost impossible to beat. It can only roll a 5 or 6"""
+
         print("You have picked 'hard' difficulty!")
         
         
+    def get_name(self):
+        """Getting the name for displaying ai difficulty"""
 
-
-
-
+        return self.name
--- src/pig/intelligence_test.py
+++ src/pig/intelligence_test.py
@@ -1,1 +1,31 @@
+"""Unit testing for intelligence"""
 
+
+# Added required imports and made test for object creation.
+# Will add more as I finish building the Intelligence class and it's methods    
+
+
+import unittest
+import intelligence
+
+
+class testIntelligenceClass(unittest.TestCase):
+    """Testing the Intelligence class"""
+
+    def test_init_default_object(self):
+        """Testing to make an object and check its properties"""
+        # Making an abject of Intelligence class
+        intelligence_object = intelligence.Intelligence()
+
+        # Testing if the object is instance of Intelligence class
+        self.assertIsInstance(intelligence_object, intelligence.Dice)
+
+
+    def test_action(self):
+        """Testing something"""
+        #Making an abject of Intelligence class
+        intelligence_object = intelligence.Intelligence()
+
+
+if __name__ == '__main__':
+    unittest.main()
